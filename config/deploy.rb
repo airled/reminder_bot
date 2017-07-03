d65c6fe9c2dff6751c58595c36860679cba697f4
airled <airled@mail.ru>
2017-07-03 18:12:46 +0300

    Changed deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,24 +2,26 @@ require 'mina/deploy'
 require 'mina/git'
 
 set :application_name, 'reminder_bot'
-set :domain, '138.201.187.144'
-set :deploy_to, '/home/reminder'
+set :domain, '88.99.226.18'
+set :deploy_to, '/home/reminder/app'
 set :repository, 'git@github.com:airled/reminder_bot.git'
 set :branch, 'master'
 set :user, 'reminder'
-set :shared_dirs, fetch(:shared_dirs, []).push('config')
+set :shared_dirs, fetch(:shared_dirs, []).push('config, deps, _build')
 
 task :environment do
 end
 
 task :setup do
+  command %{mkdir -p "/home/reminder/shared/_build"}
+  command %{mkdir -p "/home/reminder/shared/deps"}
   command %{mkdir -p "/home/reminder/shared/config"}
   command %{touch "/home/reminder/shared/config/config.exs"}
 end
 
 desc "Deploys the current version to the server."
 task :deploy do
-  invoke :'git:ensure_pushed'
+  # invoke :'git:ensure_pushed'
   deploy do
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
